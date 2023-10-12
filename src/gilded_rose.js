fd9cdc86e0f7c9a3b2bda8e1c7d255fca076672f
FrankZhou <9570075@qq.com>
2023-10-13 01:45:19 +1300

    Refactor V1

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -3,7 +3,7 @@
  * @Email: cnfrank527@gmail.com
  * @Date: 2023-10-13 00:44:07
  * @LastEditors: Frank Zhou
- * @LastEditTime: 2023-10-13 01:19:32
+ * @LastEditTime: 2023-10-13 01:40:00
  * @Description: file content
  * @FilePath: /JS/src/gilded_rose.js
  */
@@ -19,66 +19,64 @@ class Shop {
   constructor(items = []) {
     this.items = items
   }
-  updateQuality() {
-    for (var i = 0; i < this.items.length; i++) //Identity and inspect the primary structure for updateQuality function: For loop instruction is used to map each items in the shop  
-    {
-      if (
-        this.items[i].name != 'Aged Brie' &&
-        this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-      ) // Original Script here is the Logic Process when the item is Neither 'Aged Brie' Nor 'Backstage passes'
-      {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (
-            this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (
-            this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-          ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
-    }
 
-    //In conclusion, the logic of the original scripts is not clear 
+  updateQuality() {
+    this.items.forEach((item) => { ////forEach to map all items in the shop
+      switch (item.name) {
+        case 'Aged Brie':
+          this.updateAgedBrie(item); //Separate function for each sepecific item
+          break;
+        case 'Backstage passes to a TAFKAL80ETC concert':
+          this.updateBackstagePasses(item);
+          break;
+        case 'Sulfuras, Hand of Ragnaros':
+          break;
+        default:
+          this.updateStandardItem(item);
+      }
+      if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+        item.sellIn--;
+      }
+    });
+  
 
     return this.items
+  }
+
+  updateAgedBrie(item) {
+    if (item.quality < 50) {
+      item.quality++;
+    }
+    if (item.sellIn < 0 && item.quality < 50) {
+      item.quality++;
+    }
+  }
+
+  updateBackstagePasses(item) {
+    if (item.quality < 50) {
+      item.quality++;
+      if (item.sellIn < 11 && item.quality < 50) {
+        item.quality=item.quality+2;
+      }
+      if (item.sellIn < 6 && item.quality < 50) {
+        item.quality=item.quality+3;
+      }
+    }
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    }
+  }
+
+  updateStandardItem(item) {
+    if (item.quality > 0) {
+      item.quality--;
+      if (item.name === 'Conjured Mana Cake' && item.quality > 0) {
+        item.quality--; 
+      }
+    }
+    if (item.sellIn < 0 && item.quality > 0) {
+      item.quality--;
+    }
   }
 }
 module.exports = {
